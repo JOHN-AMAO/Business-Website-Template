387b0bd956d104af010554cfc24d8d0b9e05c6ab
JOHN-AMAO <amaojohn767@gmail.com>
2023-08-14 01:50:23 +0100

    diaabled strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { StyledEngineProvider } from '@mui/joy/styles';
-import App from './App';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { StyledEngineProvider } from "@mui/joy/styles";
+import App from "./App";
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
-  <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <App />
-    </StyledEngineProvider>
-  </React.StrictMode>
+  <StyledEngineProvider injectFirst>
+    <App />
+  </StyledEngineProvider>
 );
